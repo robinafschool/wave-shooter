686ae344b7b2f73e1fa41e32e61de9fce2524815
robin-afro <193246771+robin-afro@users.noreply.github.com>
2025-06-11 10:16:04 +0200

    Added shader effects

--- src/libs/moonshine/crt.lua
+++ src/libs/moonshine/crt.lua
@@ -13,13 +13,15 @@ INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
 LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
 OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 PERFORMANCE OF THIS SOFTWARE.
-]]--
+]] --
+
+local tablef = require "classes.tablef"
 
 return function(moonshine)
-  -- Barrel distortion adapted from Daniel Oaks (see commit cef01b67fd)
-  -- Added feather to mask out outside of distorted texture
-  local distortionFactor
-  local shader = love.graphics.newShader[[
+    -- Barrel distortion adapted from Daniel Oaks (see commit cef01b67fd)
+    -- Added feather to mask out outside of distorted texture
+    local distortionFactor
+    local shader = love.graphics.newShader [[
     extern vec2 distortionFactor;
     extern vec2 scaleFactor;
     extern number feather;
@@ -41,39 +43,39 @@ return function(moonshine)
     }
   ]]
 
-  local setters = {}
+    local setters = {}
 
-  setters.distortionFactor = function(v)
-    assert(type(v) == "table" and #v == 2, "Invalid value for `distortionFactor'")
-    distortionFactor = {table.unpack(v)}
-    shader:send("distortionFactor", v)
-  end
-
-  setters.x = function(v) setters.distortionFactor{v, distortionFactor[2]} end
-  setters.y = function(v) setters.distortionFactor{distortionFactor[1], v} end
-
-  setters.scaleFactor = function(v)
-    if type(v) == "table" and #v == 2 then
-      shader:send("scaleFactor", v)
-    elseif type(v) == "number" then
-      shader:send("scaleFactor", {v,v})
-    else
-      error("Invalid value for `scaleFactor'")
+    setters.distortionFactor = function(v)
+        assert(type(v) == "table" and #v == 2, "Invalid value for `distortionFactor'")
+        distortionFactor = { tablef.unpack(v) }
+        shader:send("distortionFactor", v)
     end
-  end
 
-  setters.feather = function(v) shader:send("feather", v) end
+    setters.x = function(v) setters.distortionFactor { v, distortionFactor[2] } end
+    setters.y = function(v) setters.distortionFactor { distortionFactor[1], v } end
 
-  local defaults = {
-    distortionFactor = {1.06, 1.065},
-    feather = 0.02,
-    scaleFactor = 1,
-  }
+    setters.scaleFactor = function(v)
+        if type(v) == "table" and #v == 2 then
+            shader:send("scaleFactor", v)
+        elseif type(v) == "number" then
+            shader:send("scaleFactor", { v, v })
+        else
+            error("Invalid value for `scaleFactor'")
+        end
+    end
 
-  return moonshine.Effect{
-    name = "crt",
-    shader = shader,
-    setters = setters,
-    defaults = defaults
-  }
+    setters.feather = function(v) shader:send("feather", v) end
+
+    local defaults = {
+        distortionFactor = { 1.06, 1.065 },
+        feather = 0.02,
+        scaleFactor = 1,
+    }
+
+    return moonshine.Effect {
+        name = "crt",
+        shader = shader,
+        setters = setters,
+        defaults = defaults
+    }
 end
